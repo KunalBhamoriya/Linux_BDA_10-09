eea126ec530b79a52e50b46a1d7880c530def5c6
KunalBhamoriya <kbhamoriya0@gmail.com>
2026-09-11 14:31:41 +0530

    developer 1

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 print('hello...........I am developer 2')
 print("welcome to new features")
+=======
+print('hello modified')
+print("code by dev1")
+print("welcome to bug fixing")
+>>>>>>> ccd8969 (developer 1)
